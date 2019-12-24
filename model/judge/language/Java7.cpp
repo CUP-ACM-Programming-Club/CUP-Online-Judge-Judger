be6991915840a881f13bed95dbaf9f4fa52f724f
Ryan Lee <gxlhybh@gmail.com>
2019-12-24 16:27:51 +0800

    Fix java7 compile error

--- model/judge/language/Java7.cpp
+++ model/judge/language/Java7.cpp
@@ -11,7 +11,7 @@ void Java7::run(int memory) {
     char java_xms[1u << 7];
     char java_xmx[1u << 7];
     sprintf(java_xms, "-Xmx%dM", memory);
-    sprintf(java_xmx, "-XX:MaxPermSize=%dM", mem_lmt);
+    sprintf(java_xmx, "-XX:MaxPermSize=%dM", memory);
     execl("/usr/bin/java-7", "/usr/bin/java-7", java_xms, java_xmx,
           "-Djava.security.manager",
           "-Djava.security.policy=./java.policy", "Main", (char *) nullptr);
