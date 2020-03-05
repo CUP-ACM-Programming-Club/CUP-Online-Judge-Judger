818d097b97463af0f1dba62634d9c2036786ccf8
Ryan Lee <gxlhybh@gmail.com>
2020-03-06 04:40:25 +0800

    Update build shell

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
-apt install build-essential cmake clang fp-compiler
+apt install build-essential cmake clang fp-compiler gcc g++ libgmp-dev libmysqlclient-dev
 cmake ./ && make && chmod a+x judge_client && mv ./judge_client /usr/bin
 mkdir /home/judge
 mkdir /home/judge/etc
+mkdir /dev/shm/cupoj
+mkdir /dev/shm/cupoj/submission
+useradd -m -u 1536 judge
 cp -r etc/* /home/judge/etc/
