1ee567162bd533ebc4da8b077519021abcac69bf
Ryan Lee <gxlhybh@gmail.com>
2020-03-14 23:35:16 +0800

    Update build script

--- build.sh
+++ build.sh
@@ -5,4 +5,13 @@ mkdir /home/judge/etc
 mkdir /dev/shm/cupoj
 mkdir /dev/shm/cupoj/submission
 useradd -m -u 1536 judge
-cp -r etc/* /home/judge/etc/
+cp -r etc/* /home/judge/etc/ && \
+chown -R judge /home/judge && \
+chgrp -R judge /home/judge && \
+cd model/judge/language && \
+cmake ./ && \
+make && \
+cd build/out && \
+mkdir -p /usr/lib/cupjudge && \
+cp * /usr/lib/cupjudge/ && \
+cd ../../../../../
--- upgrade.sh
+++ upgrade.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-git fetch --all && git reset --hard origin/master && cmake ./ && make && chmod a+x judge_client && mv judge_client /usr/bin
+git fetch --all && \
+git reset --hard origin/master && \
+cmake ./ && \
+make && \
+chmod a+x judge_client && \
+mv judge_client /usr/bin && \
+cd model/judge/language && \
+cmake ./ && \
+make && \
+cd build/out && \
+mkdir -p /usr/lib/cupjudge && \
+cp * /usr/lib/cupjudge/ && \
+cd ../../../../../
