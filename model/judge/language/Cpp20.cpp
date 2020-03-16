872b25ee092b62d5ba06a6e7fb136ed5ae20419e
Ryan Lee <gxlhybh@gmail.com>
2020-03-17 03:39:05 +0800

    CPP20 test support

---
+++ model/judge/language/Cpp20.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Haoyuan Li on 2020/3/17.
+//
+
+#include "Cpp20.h"
---
+++ model/judge/language/Cpp20.h
@@ -0,0 +1,16 @@
+//
+// Created by Haoyuan Li on 2020/3/17.
+//
+
+#ifndef JUDGE_LIBRARY_CPP20_H
+#define JUDGE_LIBRARY_CPP20_H
+
+
+#include "Language.h"
+
+class Cpp20: public Language {
+
+};
+
+
+#endif //JUDGE_LIBRARY_CPP20_H
