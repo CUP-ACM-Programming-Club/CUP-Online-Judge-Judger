bc0cc4964ec7b726f6a7d367a16627817a6a56f9
Ryan Lee <gxlhybh@gmail.com>
2020-04-01 01:16:25 +0800

    Update build dependency

--- shell/build_dependency.sh
+++ shell/build_dependency.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-apt install -y build-essential flex cmake clang fp-compiler gcc g++ libgmp-dev libmysqlclient-dev fp-compiler openjdk-11-jdk mono-devel
+apt install -y build-essential flex cmake clang fp-compiler gcc g++ libgmp-dev libmysqlclient-dev openjdk-11-jdk mono-devel
