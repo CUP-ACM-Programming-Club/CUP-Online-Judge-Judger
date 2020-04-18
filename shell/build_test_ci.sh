efe8858d65b5c428213947d44aa03f8550f594a2
Ryan Lee <gxlhybh@gmail.com>
2020-04-19 00:07:17 +0800

    Fix ci issue

---
+++ shell/build_test_ci.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+CPU_CORE=$(nproc)
+cmake ./ &&
+make -j$CPU_CORE &&
+cd model/judge/language &&
+cmake ./ &&
+make -j$CPU_CORE &&
+cd build/out &&
+cd ../../../../../ &&
+cd external/mysql &&
+cmake ./ &&
+make -j$CPU_CORE &&
+cd ../../ &&
+cd external/compare &&
+cmake ./ &&
+make -j$CPU_CORE &&
+cd ../../ &&
+cd sim &&
+make fresh &&
+make exes &&
+chmod +x sim* &&
+cp sim_c.exe /usr/bin/sim_c &&
+cp sim_c++.exe /usr/bin/sim_cc &&
+cp sim_java.exe /usr/bin/sim_java &&
+cp sim_pasc.exe /usr/bin/sim_pas &&
+cp sim_text.exe /usr/bin/sim_text &&
+cp sim_lisp.exe /usr/bin/sim_scm &&
+cp sim.sh /usr/bin/ &&
+cd ../
