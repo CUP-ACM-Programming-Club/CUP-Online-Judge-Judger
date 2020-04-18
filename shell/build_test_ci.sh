fc97b9b104ebfd4846316a07ccab6951f2df77e7
Ryan Lee <gxlhybh@gmail.com>
2020-04-19 00:14:04 +0800

    Remove unused ci shell

--- shell/build_test_ci.sh
+++ shell/build_test_ci.sh
@@ -19,12 +19,4 @@ cd ../../ &&
 cd sim &&
 make fresh &&
 make exes &&
-chmod +x sim* &&
-cp sim_c.exe /usr/bin/sim_c &&
-cp sim_c++.exe /usr/bin/sim_cc &&
-cp sim_java.exe /usr/bin/sim_java &&
-cp sim_pasc.exe /usr/bin/sim_pas &&
-cp sim_text.exe /usr/bin/sim_text &&
-cp sim_lisp.exe /usr/bin/sim_scm &&
-cp sim.sh /usr/bin/ &&
 cd ../
