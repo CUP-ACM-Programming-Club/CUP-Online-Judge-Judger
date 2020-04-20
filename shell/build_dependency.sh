0eed45b097a3ea3f76f7e23166715ab7cb8d56f0
Ryan Lee <gxlhybh@gmail.com>
2020-04-20 22:52:05 +0800

    Add seccomp to dependencies

--- shell/build_dependency.sh
+++ shell/build_dependency.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt install -y build-essential flex cmake libseccomp-dev clang fp-compiler gcc g++ libgmp-dev libmysqlclient-dev mono-devel libseccomp-dev
+sudo apt install -y build-essential flex cmake libseccomp-dev clang fp-compiler gcc g++ libgmp-dev libmysqlclient-dev mono-devel libseccomp-dev seccomp
