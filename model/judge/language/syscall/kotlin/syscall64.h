b6df41247150ed8e349d609daa8063d5a0e9eb1f
Ryan Lee <gxlhybh@gmail.com>
2020-04-21 22:07:17 +0800

    Update syscall64.h for kotlin

--- model/judge/language/syscall/kotlin/syscall64.h
+++ model/judge/language/syscall/kotlin/syscall64.h
@@ -4,9 +4,8 @@
 
 #ifndef JUDGE_LIBRARY_SYSCALL64_H
 #define JUDGE_LIBRARY_SYSCALL64_H
+
 #include <sys/syscall.h>
-int LANG_KTV[256] = {0, 1, 2, 3, 4, 5, 8, 9, 11, 12, 20, 21, 59, 63, 89, 158, 231, 240, 272, 511, SYS_time, SYS_read,
-                     SYS_uname, SYS_write, SYS_open,
-                     SYS_close, SYS_execve, SYS_access, SYS_brk, SYS_munmap, SYS_mprotect,
-                     SYS_mmap, SYS_fstat, SYS_set_thread_area, 252, SYS_arch_prctl, 0};
+
+int LANG_KTV[256] = {0, 1, 3, 4, 5, 9, 10, 12, 13, 14, 21, 59, 158, 202, 218, 228, 231, 257, 273, 302, 0};
 #endif //JUDGE_LIBRARY_SYSCALL64_H
