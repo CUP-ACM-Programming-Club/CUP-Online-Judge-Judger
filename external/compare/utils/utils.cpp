536522bc357e6b805f38b98575334dde316f243f
Ryan Lee <gxlhybh@gmail.com>
2020-12-13 03:51:20 +0800

    Fix dependencies.

--- external/compare/utils/utils.cpp
+++ external/compare/utils/utils.cpp
@@ -6,6 +6,9 @@
 #include <cstdlib>
 #include <cstdio>
 #include <cstring>
+#include <mutex>
+#include <condition_variable>
+#include <thread>
 #include "utils.h"
 
 int execute_cmd(const char *fmt, ...) {
@@ -17,6 +20,29 @@ int execute_cmd(const char *fmt, ...) {
     va_end(ap);
     return ret;
 }
+
+int execute_timeout_cmd(long long timeout, const char *fmt, ...) {
+    std::mutex m;
+    std::condition_variable cv;
+    int retValue;
+    char cmd[BUFFER_SIZE];
+    va_list ap;
+    va_start(ap, fmt);
+    vsprintf(cmd, fmt, ap);
+    std::thread t([&]() {
+        retValue = execute_cmd(cmd);
+        cv.notify_one();
+    });
+    t.detach();
+    {
+        std::unique_lock<std::mutex> l(m);
+        if (cv.wait_for(l, timeout * std::chrono::seconds(1)) == std::cv_status::timeout) {
+            throw std::runtime_error("Timeout");
+        }
+    }
+    return retValue;
+}
+
 
 const char *getFileNameFromPath(const char *path) {
     for (auto i = static_cast<int>(strlen(path)); i >= 0; i--) {
